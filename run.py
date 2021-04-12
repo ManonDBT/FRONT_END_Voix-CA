4f93e486b35b214f281b89c5907ae01c785bcc1d
Manon <manondbt@live.fr>
2021-04-12 11:17:45 +0200

    Modification dashboard + maj requierement

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 from flask_migrate import Migrate
-from os import environ
+
 from sys import exit
 from decouple import config
-import logging
+
 
 from config import config_dict
 from app import create_app, db
